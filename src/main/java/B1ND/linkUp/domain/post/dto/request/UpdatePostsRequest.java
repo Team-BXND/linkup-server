5852206fb1b54ea07109136d0cb31c5abb5b8706
ParkJunSeok <jdksla0129@gmail.com>
2026-01-13 20:07:13 +0900

    feat: content 및 updateRequest에 NotBlank 어노테이션 추가

--- src/main/java/B1ND/linkUp/domain/post/dto/request/UpdatePostsRequest.java
+++ src/main/java/B1ND/linkUp/domain/post/dto/request/UpdatePostsRequest.java
@@ -1,11 +1,15 @@
 package B1ND.linkUp.domain.post.dto.request;
 
 import B1ND.linkUp.domain.post.entity.Category;
+import jakarta.validation.constraints.NotBlank;
 
 public record UpdatePostsRequest(
         Category category,
+        @NotBlank(message = "제목은 필수 입력 항목입니다.")
         String title,
+        @NotBlank(message = "내용은 필수 입력 항목입니다.")
         String content,
+        @NotBlank(message = "닉네임은 필수 입력 항목입니다.")
         String author
 ) {
 }
