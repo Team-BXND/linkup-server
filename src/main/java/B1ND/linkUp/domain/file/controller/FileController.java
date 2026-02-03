d965f87c2542288e5438634752213431fc4c2ae5
ParkJunSeok <jdksla0129@gmail.com>
2026-02-03 13:20:19 +0900

    refactor: update file upload logic

--- src/main/java/B1ND/linkUp/domain/file/controller/FileController.java
+++ src/main/java/B1ND/linkUp/domain/file/controller/FileController.java
@@ -1,9 +1,14 @@
 package B1ND.linkUp.domain.file.controller;
 
+import B1ND.linkUp.domain.file.dto.request.S3Request;
 import B1ND.linkUp.domain.file.service.FileService;
 import B1ND.linkUp.global.common.APIResponse;
+import jakarta.validation.Valid;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +20,13 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileController {
     private final FileService fileService;
 
-    @PostMapping("/post")
+    @PostMapping
     public APIResponse<?> uploadPostImage(@RequestParam("file") MultipartFile file) {
         return fileService.uploadPostImage(file);
+    }
+
+    @GetMapping
+    public APIResponse<String> generatePresignedUrl(@Valid @RequestBody S3Request request) {
+        return fileService.generatePresignedUrl(request.s3key());
     }
 }
