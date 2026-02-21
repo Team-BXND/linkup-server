92fc51e30bf99df7dbdec7004f965f12ab4d3229
ParkJunSeok <jdksla0129@gmail.com>
2026-02-21 22:44:49 +0900

    feat:DockerFile, CI-CD code

--- src/main/java/B1ND/linkUp/domain/file/controller/FileController.java
+++ src/main/java/B1ND/linkUp/domain/file/controller/FileController.java
@@ -1,9 +1,7 @@
 package B1ND.linkUp.domain.file.controller;
 
-import B1ND.linkUp.domain.file.dto.request.S3Request;
 import B1ND.linkUp.domain.file.service.FileService;
 import B1ND.linkUp.global.common.APIResponse;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -25,7 +23,7 @@ public class FileController {
     }
 
     @GetMapping
-    public APIResponse<String> generatePresignedUrl(@Valid @RequestBody S3Request request) {
-        return fileService.generatePresignedUrl(request.s3key());
+    public APIResponse<String> generatePresignedUrl(@RequestParam String s3Key) {
+        return fileService.generatePresignedUrl(s3Key);
     }
 }
